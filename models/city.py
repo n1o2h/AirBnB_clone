f5dec30b5bb5061a5dc2e3601d2e25e0c7f45092
n1o2h <aythmadnhylt@gmail.com>
2024-02-10 15:31:38 -0800

    updated

--- models/city.py
+++ models/city.py
@@ -3,6 +3,6 @@
 from models.base_model import BaseModel
 
 class City(BaseModel):
-   
+
     name = ""
     state_id = ""
--- models/state.py
+++ models/state.py
@@ -3,5 +3,5 @@
 from models.base_model import BaseModel
 
 class State(BaseModel):
-   
+
     name = ""
