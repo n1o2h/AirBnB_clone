1f1c453512144f9d1175a7198bf060adc41df5cc
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-02-12 03:00:51 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" Amenity file"""
+
 from models.base_model import BaseModel
-""" import section"""
+
 class Amenity(BaseModel):
-""" the class documentation"""
+
     name = ""
