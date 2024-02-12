2861d4c9d13262bfce66267619fa4cfcb49a5546
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-02-12 02:48:20 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+""" City file"""
 from models.base_model import BaseModel
-
+""" import section"""
 class City(BaseModel):
-
+""" the class documentation"""
     name = ""
     state_id = ""
