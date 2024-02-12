5379e5f9cdb85eb1a5e753a9bb9f463cd8bb353d
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-02-12 02:51:17 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,7 +1,9 @@
 #!usr/bin/python3
+""" user file"""
 from models.base_model import BaseModel
-
+""" import section"""
 class User(BaseModel):
+""" the class documentation"""
     email = ""
     password = ""
     first_name = ""
