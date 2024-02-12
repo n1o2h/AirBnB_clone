05e9e25e4636288b006069f3c04f1eb1436a7427
SABRI <132160031+Chihab2023@users.noreply.github.com>
2024-02-12 03:04:16 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-""" Review file"""
 from models.base_model import BaseModel
-""" import section"""
 class Review(BaseModel):
-""" the class documentation"""
     text = ""
     user_id = ""
     place_id = ""
